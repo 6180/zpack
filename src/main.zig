4d2848a2d929cd738d1a428b915a63aa0cc0d56a
czyzx <quine.618033988@gmail.com>
2021-12-13 00:04:43 -0500

    Bunch od stuff, bad at git.  Realloc fns, pack fns for posfixint, uint8, bool, posnegint,

--- src/main.zig
+++ src/main.zig
@@ -10,7 +10,16 @@ pub fn main() anyerror!void {
     defer mps.deinit();
 
     _ = try mps.packBool(true);
+    _ = try mps.packPosFixint(0);
+    _ = try mps.packNegFixint(23);
+    _ = try mps.packNil();
+    _ = try mps.packPosFixint(112);
+    _ = try mps.packNegFixint(12);
     _ = try mps.packBool(false);
+    _ = try mps.packBool(false);
+    _ = try mps.packUint8(128);
+    _ = try mps.packUint8(42);
+
     mps.dump();
 
     // std.log.info("Type of `noomba` is {s}", .{@typeName(@TypeOf(noomba))});
@@ -34,7 +43,7 @@ fn beCast(dest_type: type, n: anytype) !dest_type {
         else => if (@TypeOf(n) == dest_type) n else error{IncompatibleTypes},
     };
 }
- 
+
 // fn beCastSafe(dest_type: type, n: anytype) !dest_type {
 
 // }
@@ -42,75 +51,153 @@ fn beCast(dest_type: type, n: anytype) !dest_type {
 /// Writes a native int with BE byte ordering.
 /// Returns: number of bytes written.
 const MsgpackStream = struct {
-    ator: *Allocator = undefinedntype,
+    ator: *Allocator = undefined,
     buf: []u8 = undefined,
     capacity: usize = undefined,
     // end: usize = 0,
     pos: usize = 0,
-wqdwdqwdqwdqwd
     /// Prints type and value of all objects in this stream.
     pub fn dump(mps: MsgpackStream) void {
         var i: usize = 0;
+
+        _ = std.log.info("", .{});
+        _ = std.log.info("MsgpackStream dump:", .{});
+        _ = std.log.info("Capacity: {d}", .{mps.capacity});
+        _ = std.log.info("Pos: {d}", .{mps.pos});
+
         while (i < mps.pos) {
             var tag: u8 = mps.buf[i];
+            i += 1;
 
             switch (tag) {
-                0xC2 => {
-                    std.log.info("Bool: False", .{});
+                0x00...0x7F => std.log.info("Positive Fixint: {d}", .{tag}),
+                0xC0 => std.log.info("nil", .{}),
+                0xC2 => std.log.info("Bool: False", .{}),
+                0xC3 => std.log.info("Bool: True", .{}),
+                0xCC => {
+                    std.log.info("Uint8: {d}", .{mps.buf[i]});
                     i += 1;
                 },
-                0xC3 => {
-                    std.log.info("Bool: True", .{});
-                    i += 1;
-                },
+                0xE0...0xFF => std.log.info("Negative Fixint: -{d}", .{tag & 0b0001_1111}),
                 else => std.log.info("Unknown tag: {X}", .{tag}),
             }
         }
     }
 
-    pub fn packNil(mps: *MsgpackStream) anyerror!void {
+    // fn checkFit(mps: MsgpackStream, obj: anytype) {
+
+    // }
+    pub fn realloc(mps: *MsgpackStream, new_capacity: usize) !void {
+        var new_buf = try mps.ator.alloc(u8, new_capacity);
+        std.mem.copy(u8, new_buf, mps.buf[0..mps.pos]);
+        mps.ator.free(mps.buf);
+        mps.buf = new_buf;
+        mps.capacity = new_capacity;
+    }
+
+    pub fn reallocIfNeeded(mps: *MsgpackStream, desired_capacity: usize) !void {
+        if (desired_capacity > mps.capacity) {
+            var new_cap = mps.capacity * 2;
+            _ = try mps.realloc(new_cap);
+        } else {
+            mps.capacity = desired_capacity;
+        }
+    }
+
+    /// Writes a 1 byte nil to the object stream.
+    /// Nil: 0xC0
+    /// Returns number of bytes written (always 1)
+    pub fn packNil(mps: *MsgpackStream) !usize {
+        _ = try mps.reallocIfNeeded(mps.pos + 1);
+
+        std.log.info("pos: {d}, cap: {d}, NIL", .{ mps.pos, mps.capacity });
+
         mps.buf[mps.pos] = 0xC0;
         mps.pos += 1;
+        return 1;
     }
+
     /// Writes a 1 byte bool to the object stream.
     /// False: 0xC2, True: 0xC3
     /// Returns number of bytes written (always 1)
     pub fn packBool(mps: *MsgpackStream, b: bool) !usize {
+        _ = try mps.reallocIfNeeded(mps.pos + 1);
+
+        std.log.info("pos: {d}, cap: {d}, BOOL", .{ mps.pos, mps.capacity });
+
         mps.buf[mps.pos] = @as(u8, if (b) 0xC3 else 0xC2);
         mps.pos += 1;
         return 1;
     }
+
+    /// Writes a 7 bit positive integer to the object stream.
+    /// Returns number of bytes written (always 1).
+    pub fn packPosFixint(mps: *MsgpackStream, n: u7) !usize {
+        _ = try mps.reallocIfNeeded(mps.pos + 1);
+
+        std.log.info("pos: {d}, cap: {d}, POSFIXINT", .{ mps.pos, mps.capacity });
+
+        mps.buf[mps.pos] = @as(u8, n);
+        mps.pos += 1;
+        return 1;
+    }
+
+    /// Writes a 5 bit negative integer to the object stream.
+    /// Returns number of bytes written (always 1).
+    pub fn packNegFixint(mps: *MsgpackStream, n: u5) !usize {
+        _ = try mps.reallocIfNeeded(mps.pos + 1);
+
+        std.log.info("pos: {d}, cap: {d}, NEGFIXINT", .{ mps.pos, mps.capacity });
+
+        mps.buf[mps.pos] = @as(u8, n) | @as(u8, 0b1110_0000);
+        mps.pos += 1;
+        return 1;
+    }
+
+    /// Writes an 8-bit unsigned integer to the object stream.
+    /// Returns number of bytes written (always 2).
+    pub fn packUint8(mps: *MsgpackStream, n: u8) !usize {
+        _ = try mps.reallocIfNeeded(mps.pos + 2);
+
+        std.log.info("pos: {d}, cap: {d}, UINT8", .{ mps.pos, mps.capacity });
+
+        mps.buf[mps.pos] = 0xCC;
+        mps.pos += 1;
+        mps.buf[mps.pos] = n;
+        mps.pos += 1;
+        return 2;
+    }
+
     /// Writes a native int with BE byte ordering to the stream.
     /// Returns: number of bytes written.
-    pub fn PackUint(mps: MsgpackStream, n: anytype) !usize {
-        var num = n;
-        const num_ti = @typeInfo(num);
-        const endianess = Arch.endian();
+    // pub fn PackUint(mps: MsgpackStream, n: anytype) !usize {
+    //     var num = n;
+    //     const num_ti = @typeInfo(num);
+    //     const endianess = Arch.endian();
 
-        // Make sure n is an int.
-        if (num_ti != .Int)
-            return error{TypeError};
+    //     // Make sure n is an int.
+    //     if (num_ti != .Int)
+    //         return error{TypeError};
 
-        const is_signed: bool = (num_ti.signedness == .signed);
+    //     const is_signed: bool = (num_ti.signedness == .signed);
 
-        // If arch is LE, swap byte order.
-        if (endianess == .Little)
-            num = @byteSwap(@TypeOf(n), num);
+    //     // If arch is LE, swap byte order.
+    //     if (endianess == .Little)
+    //         num = @byteSwap(@TypeOf(n), num);
 
-        // const bit_width = @bitSizeOf(num);
+    //     // const bit_width = @bitSizeOf(num);
 
-        if (is_signed and num < 0 and num >= -32) {
-            // Negative fixint stores a 5 bit negative number. [ 111YYYYY ]
-            mps.buf[mps.pos] = @intCast(u8, num) & @as(u8, 0b1110_0000);
-            mps.pos += 1;
-        }
-    }
+    //     if (is_signed and num < 0 and num >= -32) {
+    //         // Negative fixint stores a 5 bit negative number. [ 111YYYYY ]
+    //         mps.buf[mps.pos] = @intCast(u8, num) & @as(u8, 0b1110_0000);
+    //         mps.pos += 1;
+    //     }
+    // }
 
     pub fn init(alligator: *Allocator) !MsgpackStream {
         var buff: []u8 = undefined;
-        const cap: usize = 64;
+        const cap: usize = 2;
         buff = try alligator.alloc(u8, cap); // XXX: magic
-
 
         return MsgpackStream{
             .ator = alligator,
